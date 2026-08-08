cad0a0e222149544009bf106d2d289b9b8fcd846
ajinkya446 <ajinkya446@gmail.com>
2026-08-08 23:40:22 +0530

    chore: updated gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_assignment/app/my_app.dart';
 import 'package:flutter_assignment/core/storage/star_war_database.dart';
-import 'package:flutter_assignment/firebase_options.dart' show DefaultFirebaseOptions;
+import 'package:flutter_assignment/firebase_options.dart';
 import 'package:flutter_assignment/injection_container.dart';
-
-import 'app/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   /// Opening and creation of SQFLite database
   await DatabaseHelper.instance.database;
