57f7f14a6c58ad479b9ee59b9accf04110d92ef9
ajinkya446 <ajinkya446@gmail.com>
2026-08-08 19:11:16 +0530

    feat: modernize app with dependency upgrades, Material 3 UI overhaul, and Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_assignment/core/storage/star_war_database.dart';
+import 'package:flutter_assignment/firebase_options.dart';
 
 import 'app/my_app.dart';
 import 'injection_container.dart';
 
-// part 'main.main.dart';
-// part 'main_model.g.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   /// Opening and creation of SQFLite- database
   await DatabaseHelper.instance.database;
